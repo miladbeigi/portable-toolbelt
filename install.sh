d78d852d84fa70c2f72678795728a2a3b12fea33
miladbeigi <milad.be@gmail.com>
2025-08-04 10:15:33 -0400

    fix: update paths

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 set -e
 
 # --------- Load Core Utilities ----------
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/src/core/detect_os.sh"
+cd ~/.local/share/portable-toolbelt/
+source src/core/detect_os.sh
 
 # --------- OS Detection --------------
 detect_os
